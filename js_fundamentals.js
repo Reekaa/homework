d80d8a1c2e411cb9b189fd60015c935124de33d1
Reka <a9886177@9886177s-MacBook-Pro.local>
2019-04-02 16:47:45 +0100

    var fName

---
+++ js_fundamentals.js
@@ -0,0 +1,44 @@
+7 + 3;
+10
+
+9 % 2;
+1
+
+typeof(42);
+'number'
+
+typeof(2) === typeof(9.24);
+true
+
+42 / 0;
+Infinity
+
+9 / "player1";
+NaN
+
+NaN === NaN;
+false
+
+typeof(NaN);
+undefined
+
+typeof parseInt("42");
+number
+
+"Brendan" + " Eich";
+Brendan Eich
+
+var firstName = "Brendan";
+var lastName = "Eich";
+firstName + " " + lastName;
+Brendan Eich
+
+var phoneNumber = "08798976";
+phoneNumber.length;
+????
+
+"Brendan" === "Brendan";
+true
+
+var fName;
+if (fName) console.log(`First name is ${fName}`);
