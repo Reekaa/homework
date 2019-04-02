dd96def8e316ab8894d86c54b871e23171cf2259
Reka <a9886177@9886177s-MacBook-Pro.local>
2019-04-02 19:38:15 +0100

    homework_final

--- js_fundamentals.js
+++ js_fundamentals.js
@@ -1,44 +1,100 @@
-7 + 3;
-10
+// // Excercise 1
+// 7 + 3;
+// 10
+//
+// 9 % 2;
+// 1
+//
+// typeof(42);
+// 'number'
+//
+// typeof(2) === typeof(9.24);
+// true
+//
+// 42 / 0;
+// Infinity
+//
+// 9 / "player1";
+// NaN
+//
+// NaN === NaN;
+// false
+//
+// typeof(NaN);
+// number
+//
+// typeof parseInt("42");
+// number
+//
+// "Brendan" + " Eich";
+// Brendan Eich
+//
+// var firstName = "Brendan";
+// var lastName = "Eich";
+// firstName + " " + lastName;
+// Brendan Eich
+//
+// var phoneNumber = "08798976";
+// phoneNumber.length;
+// 8
+//
+// "Brendan" === "Brendan";
+// true
+//
+// var fName;
+// if (fName) console.log(`First name is ${fName}`);
+// no
+//
+// var name = "";
+// if (name) console.log(`First name is $(name)`);
+// no
 
-9 % 2;
-1
+// Exercise 2
 
-typeof(42);
-'number'
+// var score = 10;
+// switch (score) {
+//   case 8:
+//   console.log('Pass');
+//     break;
+//   case 9:
+//   console.log('Merit');
+//     break;
+//   case 10:
+//   console.log('Distinction');
+//     break;
+//   default:
+//   console.log('Fail');
+// }
 
-typeof(2) === typeof(9.24);
-true
+// Excercise 3
 
-42 / 0;
-Infinity
+// var coffees = [
+//   { type:"Latte", price:2.65 },
+//   { type:"Cappuccino", price:2.75 },
+//   { type:"Flat white", price:2.95 },
+//   { type:"Super-dooper Mocha Deluxe", price:3.79 }
+// ];
+//
+// for (coffee of coffees) {
+//   console.log(coffee.type + " " + "cost" + " " + coffee.price)
+// }
 
-9 / "player1";
-NaN
+// Excercise 4 - I wasn't sure what to do with this, the below idea came from the team...
 
-NaN === NaN;
-false
-
-typeof(NaN);
-undefined
-
-typeof parseInt("42");
-number
-
-"Brendan" + " Eich";
-Brendan Eich
-
-var firstName = "Brendan";
-var lastName = "Eich";
-firstName + " " + lastName;
-Brendan Eich
-
-var phoneNumber = "08798976";
-phoneNumber.length;
-????
-
-"Brendan" === "Brendan";
-true
-
-var fName;
-if (fName) console.log(`First name is ${fName}`);
+// var currentPlayer = 'x';
+// var grid = [['x','x','x'], ['o', null, 'o'], [null, null, null]];
+//
+//
+// if ((grid[0][0]===currentPlayer&&grid[0][1]===currentPlayer&&grid[0][2]===currentPlayer)||
+//     (grid[0][0]===currentPlayer&&grid[1][1]===currentPlayer&&grid[2][2]===currentPlayer)||
+//     (grid[1][0]===currentPlayer&&grid[1][1]===currentPlayer&&grid[1][2]===currentPlayer)||
+//     (grid[0][2]===currentPlayer&&grid[1][1]===currentPlayer&&grid[2][0]===currentPlayer)||
+//     (grid[0][1]===currentPlayer&&grid[1][1]===currentPlayer&&grid[2][1]===currentPlayer)||
+//     (grid[0][2]===currentPlayer&&grid[1][2]===currentPlayer&&grid[2][2]===currentPlayer)||
+//     (grid[2][0]===currentPlayer&&grid[2][1]===currentPlayer&&grid[2][2]===currentPlayer))
+// {
+//   console.log("x wins");
+// }
+// else {
+//   console.log("No winner");
+// }
